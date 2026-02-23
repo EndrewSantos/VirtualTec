b5fcf330e2b1d5e2c7c535ece1500a0f41ab3a1f
EndrewSantos <endrewdossantosrafael@gmail.com>
2026-02-23 17:26:07 -0300

    Primeiro commit - VirtualTec

---
+++ _next/static/YNtjITRm_N1JMHjX8Mdhf/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/virtualtec//_next/:path+",
+        "destination": "/virtualtec/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
